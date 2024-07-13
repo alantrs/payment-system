66357444c318139a71b1761759b8df005ce84288
alantrs <alanaraujotrs@gmail.com>
2024-07-13 14:51:36 -0300

    Envio de email de verificação

--- src/main/java/com/payment/paymentsystem/controller/UserController.java
+++ src/main/java/com/payment/paymentsystem/controller/UserController.java
@@ -3,13 +3,13 @@ package com.payment.paymentsystem.controller;
 import com.payment.paymentsystem.dto.UserRequest;
 import com.payment.paymentsystem.dto.UserResponse;
 import com.payment.paymentsystem.service.UserService;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/users")
@@ -22,8 +22,16 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest user) {
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest user) throws MessagingException, UnsupportedEncodingException {
         UserResponse userSaved = userService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userSaved);
+    }
+
+    @GetMapping("verify")
+    public ResponseEntity<String> verify(@RequestParam Integer code){
+        if (userService.verify(code)){
+            return ResponseEntity.status(HttpStatus.OK).body("User verified");
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User not verified");
     }
 }
