2c42f5cf696556317f744ee18185b4d4a25e8936
alantrs <alanaraujotrs@gmail.com>
2024-07-15 21:54:44 -0300

    Método de criação e validação de token JWT

---
+++ src/main/java/com/payment/paymentsystem/service/TokenService.java
@@ -0,0 +1,50 @@
+package com.payment.paymentsystem.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.payment.paymentsystem.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${payment_system.security.token.secret")
+    private String secret;
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("payment_system")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException exception) {
+            throw new RuntimeException("ERROR: Error while generating token", exception);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("payment_system")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception) {
+            throw new RuntimeException("ERROR: Token invalid", exception);
+        }
+    }
+
+    private Instant genExpirationDate(){
+        return LocalDateTime.now().plusMinutes(30).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
