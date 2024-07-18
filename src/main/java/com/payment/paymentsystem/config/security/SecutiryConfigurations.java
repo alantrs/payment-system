beac5e13e13369f6b3088d92fef145552f303956
alantrs <alanaraujotrs@gmail.com>
2024-07-17 22:02:46 -0300

    segurança

--- src/main/java/com/payment/paymentsystem/config/security/SecutiryConfigurations.java
+++ src/main/java/com/payment/paymentsystem/config/security/SecutiryConfigurations.java
@@ -1,13 +1,60 @@
 package com.payment.paymentsystem.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class SecutiryConfigurations {
 
+    @Autowired
+    SecurityFilter securityFilter;
+
+    private static final String[] WHITELIST_SWAGGER = {
+            "/api/v1/auth/",
+            "/v3/api-docs/**",
+            "/v3/api-docs.yaml",
+            "/swagger-ui/**",
+            "/swagger-ui/index.html",
+            "/swagger-ui.html"
+    };
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> {
+                    for (String url : WHITELIST_SWAGGER) {
+                        authorize.requestMatchers(antMatcher(url)).permitAll();
+                    }
+                    authorize
+                            .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/users/verify").permitAll()
+                            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                            .anyRequest().authenticated();
+                }).
+
+                addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
--- src/main/java/com/payment/paymentsystem/controller/UserController.java
+++ src/main/java/com/payment/paymentsystem/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.payment.paymentsystem.controller;
 
+import com.payment.paymentsystem.dto.LoginDTO;
 import com.payment.paymentsystem.dto.UserRequest;
 import com.payment.paymentsystem.dto.UserResponse;
+import com.payment.paymentsystem.entity.User;
+import com.payment.paymentsystem.service.TokenService;
 import com.payment.paymentsystem.service.UserService;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
@@ -17,17 +23,18 @@ public class UserController {
 
     private final UserService userService;
 
+
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest user) throws MessagingException, UnsupportedEncodingException {
         UserResponse userSaved = userService.createUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userSaved);
     }
 
-    @GetMapping("verify")
+    @GetMapping("/verify")
     public ResponseEntity<String> verify(@RequestParam Integer code){
         if (userService.verify(code)){
             return ResponseEntity.status(HttpStatus.OK).body("User verified");
